41de7e39e6e5c0e5f5179352baba1ceab6e1861b
Jeff Byrnes <jeff@evertrue.com>
2015-07-31 16:36:36 -0400

    Bump to v2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs Singularity dependencies (mesos, zk, mysql)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '2.0.0'
 supports         'ubuntu'
 
 depends 'apt'
