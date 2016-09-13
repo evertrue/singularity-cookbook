121b751d22aec62e4febe4bd642c653e9f1d8788
Eric Herot <eric.github@herot.com>
2016-09-13 14:03:13 -0400

    Version bump 10.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+singularity CHANGELOG
+This is the Changelog for the singularity cookbook.
+
+## [Unreleased][unreleased]
+
+### Changes
+
+### Fixes
+
+## v10.0.0 - (2016-09-13)
+
+## Changes
+
+* Add a CHANGELOG!
+* Break out the Singularity Executor into its own [cookbook](https://github.com/evertrue/singularity_executor-cookbook)
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs Singularity and its dependencies (mesos, zk, mysql)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '9.0.2'
+version          '10.0.0'
 supports         'ubuntu'
 
 depends 'apt'
